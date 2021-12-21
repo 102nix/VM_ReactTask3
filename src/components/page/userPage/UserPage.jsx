9d492cd949c705d6118988b74548c45843eeb6a4
kostya zalilov <zalilov.k@gmail.com>
2021-12-21 21:18:45 +0500

    lessons17

--- src/components/page/userPage/UserPage.jsx
+++ src/components/page/userPage/UserPage.jsx
@@ -1,17 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import api from '../../../api/index'
+import React from 'react'
 import { UserCard } from '../../ui/UserCard'
 import { QualitiesCard } from '../../ui/QualitiesCard'
 import { MeetingsCard } from '../../ui/MeetingsCard'
 import { Comments } from '../../ui/Comments'
+import { useUser } from '../../../hooks/useUsers'
 
 export const UserPage = ({ userId }) => {
-  const [user, setUser] = useState()
-
-  useEffect(() => {
-    api.users.getById(userId).then((data) => setUser(data))
-  }, [])
-
+  const { getUserById } = useUser()
+  const user = getUserById(userId)
+  console.log('UserPage:', user)
   return (
     <>
       {user ? (
