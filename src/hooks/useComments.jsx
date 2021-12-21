b20dc15c2eca23083c3cebcbe7573336a5dbd732
kostya zalilov <zalilov.k@gmail.com>
2021-12-22 01:54:02 +0500

    lessons22 done

--- src/hooks/useComments.jsx
+++ src/hooks/useComments.jsx
@@ -20,8 +20,7 @@ export const CommentsProvider = ({ children }) => {
   const [error, setError] = useState(null)
 
   useEffect(() => {
-    setComments(null)
-    setLoading(false)
+    getComments()
   }, [])
   async function createComment (data) {
     const comment = {
@@ -38,6 +37,17 @@ export const CommentsProvider = ({ children }) => {
       errorCatcher(error)
     }
     console.log(comment)
+  }
+  async function getComments () {
+    try {
+      const { content } = await commentService.getComments(userId)
+      console.log(content)
+      setComments(content)
+    } catch (error) {
+      errorCatcher(error)
+    } finally {
+      setLoading(false)
+    }
   }
   function errorCatcher (error) {
     const { message } = error.response.data
