91e0b42f0a101f22c4ed97829d0ce19c298641ad
kostya zalilov <zalilov.k@gmail.com>
2021-12-22 00:46:55 +0500

    lessons19 done

--- src/components/page/userPage/UserPage.jsx
+++ src/components/page/userPage/UserPage.jsx
@@ -4,6 +4,7 @@ import { QualitiesCard } from '../../ui/QualitiesCard'
 import { MeetingsCard } from '../../ui/MeetingsCard'
 import { Comments } from '../../ui/Comments'
 import { useUser } from '../../../hooks/useUsers'
+import { CommentsProvider } from '../../../hooks/useComments'
 
 export const UserPage = ({ userId }) => {
   const { getUserById } = useUser()
@@ -20,7 +21,9 @@ export const UserPage = ({ userId }) => {
               <MeetingsCard value={user.completedMeetings} />
             </div>
             <div className="col-md-8">
-              <Comments />
+              <CommentsProvider>
+                <Comments />
+              </CommentsProvider>
             </div>
           </div>
         </div>
